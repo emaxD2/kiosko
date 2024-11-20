41cfc60aeb4df67bc506688055be43ff8c20218b
emaxD2 <ecarpioa_cb@est.emi.edu.bo>
2024-11-19 20:59:44 -0400

    Primer commit: Proyecto Kiosko

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { initializeApp } from "firebase/app";
+// Importa solo Analytics si lo necesitas
+import { getAnalytics } from "firebase/analytics";
+import { environment } from './environments/environment';
+
+// Inicializar Firebase
+const app = initializeApp(environment.firebase);
+
+// Inicializar Analytics (opcional)
+if (process.env.NODE_ENV === 'production' || location.hostname === 'localhost') {
+  const analytics = getAnalytics(app);
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
 root.render(
   <React.StrictMode>
     <App />

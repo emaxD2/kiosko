7975a382901485a9614693a0d9feb85ff719c393
emaxD2 <ecarpioa_cb@est.emi.edu.bo>
2025-01-03 12:27:59 -0400

    cambios menores

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { initializeApp } from "firebase/app";
-// Importa solo Analytics si lo necesitas
-import { getAnalytics } from "firebase/analytics";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
